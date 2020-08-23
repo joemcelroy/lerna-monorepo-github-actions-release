0f621ffc85d82d09a7fce2262bf60da79601e381
Joseph McElroy <phoey1@gmail.com>
2020-08-23 20:20:16 +0100

    docs

---
+++ packages/mono-docs/.docusaurus/client-modules.js
@@ -0,0 +1,7 @@
+export default [
+  require("/Users/j.mcelroy/projects/lerna-monorepo-github-actions-release/node_modules/remark-admonitions/styles/infima.css"),
+  require("/Users/j.mcelroy/projects/lerna-monorepo-github-actions-release/node_modules/remark-admonitions/styles/infima.css"),
+  require("/Users/j.mcelroy/projects/lerna-monorepo-github-actions-release/node_modules/remark-admonitions/styles/infima.css"),
+  require("/Users/j.mcelroy/projects/lerna-monorepo-github-actions-release/node_modules/infima/dist/css/default/default.css"),
+  require("/Users/j.mcelroy/projects/lerna-monorepo-github-actions-release/node_modules/@docusaurus/theme-classic/src/prism-include-languages"),
+];
---
+++ packages/mono-docs/.docusaurus/routes.js
@@ -0,0 +1,41 @@
+
+import React from 'react';
+import ComponentCreator from '@docusaurus/ComponentCreator';
+export default [
+{
+  path: '/',
+  component: ComponentCreator('/','deb'),
+  exact: true,
+},
+{
+  path: '/docs',
+  component: ComponentCreator('/docs','498'),
+  
+  routes: [
+{
+  path: '/docs/',
+  component: ComponentCreator('/docs/','87d'),
+  exact: true,
+},
+{
+  path: '/docs/doc2',
+  component: ComponentCreator('/docs/doc2','fd3'),
+  exact: true,
+},
+{
+  path: '/docs/doc3',
+  component: ComponentCreator('/docs/doc3','e02'),
+  exact: true,
+},
+{
+  path: '/docs/mdx',
+  component: ComponentCreator('/docs/mdx','955'),
+  exact: true,
+},
+]
+},
+{
+  path: '*',
+  component: ComponentCreator('*')
+}
+];
